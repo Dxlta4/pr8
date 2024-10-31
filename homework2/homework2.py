4054bcc48550cb2c0f8e2d9c1366f94b1ea2ca10
Dxlta4 <mistickplay@gmail.com>
2024-10-31 19:01:13 +0500

    Добавил homework2

---
+++ homework2/homework2.py
@@ -0,0 +1,7 @@
+while True:
+    try:
+        num1 = int(input("Введите первое число: "))
+        num2 = int(input("Введите второе число: "))
+        print("Сумма:", num1 + num2)
+    except ValueError:
+        print("Ошибка: пожалуйста, введите целые числа.")
